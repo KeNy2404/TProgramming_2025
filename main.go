3c4ca377f9f251a77a172c19e2fc25c395bc405c
Пользователь <Aleksandr24441@gmail.com>
2025-05-19 01:27:00 +0300

    Решение задачи А и В с помощью циклов и массивов

--- main.go
+++ main.go
@@ -14,6 +14,17 @@ func main() {
 	fmt.Println("x4 = ", calculate(1.5))
 	fmt.Println("x5 = ", calculate(2.3))
 
+	fmt.Println("Решение задачи А")
+
+	for i := 0.2; i <= 2.2; i += 0.1 {
+		fmt.Println("x", i, " = ", calculate(i))
+	}
+
+	fmt.Println("Решение задачи Б")
+	x := []float64{0.1, 0.9, 1.2, 1.5, 2.3}
+	for i, value := range x {
+		fmt.Println("x", i, " = ", calculate(value))
+	}
 }
 
 func calculate(x float64) float64 {
